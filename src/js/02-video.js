e94a1a7160df86eff09b33fc025be659556d6533
Anastasiia Kazymko <92369437+AnastasiiaKazymko@users.noreply.github.com>
2023-08-11 20:50:28 +0300

    add video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const currentTime = {
+  duration: null,
+  percent: null,
+  seconds: null,
+};
+
+player.on('timeupdate', function (data) {
+  currentTime.duration = data.duration;
+  currentTime.percent = data.percent;
+  currentTime.seconds = data.seconds;
+});
