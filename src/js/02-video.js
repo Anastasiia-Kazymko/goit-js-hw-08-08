93680ba03b5b7835c18925bf4308e5314004daf7
Anastasiia Kazymko <92369437+AnastasiiaKazymko@users.noreply.github.com>
2023-08-16 21:34:55 +0300

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,4 +1,5 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
@@ -13,4 +14,12 @@ player.on('timeupdate', function (data) {
   currentTime.duration = data.duration;
   currentTime.percent = data.percent;
   currentTime.seconds = data.seconds;
+  const currentTimeJSON = JSON.stringify(currentTime);
+  throttle(localStorage.setItem('CurrentTime', currentTimeJSON), 1000);
+  if (currentTimeJSON) {
+    currentTime.duration = data.duration;
+    currentTime.percent = data.percent;
+    currentTime.seconds = data.seconds;
+  }
+  console.log(currentTimeJSON);
 });
