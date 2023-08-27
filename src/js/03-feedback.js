06a348b2467cdefb962a43faf6a986a810107c14
Anastasiia Kazymko <92369437+AnastasiiaKazymko@users.noreply.github.com>
2023-08-27 21:06:59 +0300

    add change

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,23 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('textarea'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', onTextareaInput);
+
+populateTextArea();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  e.currentTarget.reset();
+}
+
+function onTextareaInput(e) {
+  const message = e.target.value;
+  localStorage.setItem('feedback-msg', message);
+}
+
+function populateTextArea() {}
