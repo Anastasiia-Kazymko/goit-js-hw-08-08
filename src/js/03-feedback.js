4b458598cd6c198d228205c80aabf323ff1adfa3
Anastasiia Kazymko <92369437+AnastasiiaKazymko@users.noreply.github.com>
2023-09-19 20:46:35 +0300

    add 3 task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,23 +1,35 @@
 import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+
+const formData = {};
+
 const refs = {
   form: document.querySelector('.feedback-form'),
   textarea: document.querySelector('textarea'),
 };
 
 refs.form.addEventListener('submit', onFormSubmit);
-refs.textarea.addEventListener('input', onTextareaInput);
+refs.form.addEventListener('input', throttle(onTextareaInput, 500));
 
 populateTextArea();
 
 function onFormSubmit(e) {
   e.preventDefault();
   e.currentTarget.reset();
+  localStorage.removeItem(STORAGE_KEY);
 }
 
 function onTextareaInput(e) {
-  const message = e.target.value;
-  localStorage.setItem('feedback-msg', message);
+  formData[e.target.name] = e.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
 
-function populateTextArea() {}
+function populateTextArea() {
+  const savedFormData = localStorage.getItem(STORAGE_KEY);
+  if (savedFormData) {
+    const parselFormData = JSON.parse(savedFormData);
+    console.log(refs.form);
+    refs.textarea.value = parselFormData.message;
+  }
+}
